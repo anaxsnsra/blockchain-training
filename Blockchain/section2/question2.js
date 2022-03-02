a5d258a880a332432163bf35a1c84e133297930a
muhammad anas <anaxcool95@yahoo.com>
2022-03-02 22:06:16 +0800

    update section 2 question 2

--- Blockchain/section2/question2.js
+++ Blockchain/section2/question2.js
@@ -41,14 +41,11 @@ class DevAccount extends Account{
         }       
     }
 
-    transfer(balance, amount, newAccBalance){
-        if(balance > amount){
-            var transferAccAmount = amount;
-            this.balance = balance;
-            var balance = this.withdraw(amount);
-            newAccBalance = newAccBalance + transferAccAmount;
-            return "Success transfer : " + transferAccAmount + 
-            " and " + this.name + " Account balance is : " + balance
+    transfer(acc1,acc2, amount){
+        if(acc1.balance > amount){
+            var balance = acc1.withdraw(amount);
+            var newAccBalance = acc2.deposit(amount);
+            return acc1.name + " new balance : " + balance + " and " + acc2.name + " new balance : " + newAccBalance
         }
         else{
             return "not enough balance"
@@ -57,24 +54,22 @@ class DevAccount extends Account{
 }
 
 var newAcc = new DevAccount("Anas",1000)
-newAcc.deposit(500)
-newAcc.setBalance("Anas",1000)
-
 var newAcc2 = new DevAccount("Amid",500)
-newAcc2.deposit(10)
 
-newAcc.transfer(newAcc.getBalance("Anas"),500,newAcc2.balance)
+newAcc.transfer(newAcc,newAcc2,500)
+newAcc.setBalance(newAcc.name,500)
+newAcc2.setBalance(newAcc2.name,50)
 
-var getBalance = newAcc.getBalance("Anas");
-var getBalance2 = newAcc2.getBalance("Amid");
+var balanceAcc1 = newAcc.getBalance(newAcc.name)
+var balanceAcc2 = newAcc2.getBalance(newAcc2.name)
 
-console.log(getBalance)
-console.log(getBalance2)
+console.log("Get " + newAcc.name + " balance : " + balanceAcc1)
+console.log("Get " + newAcc2.name + " balance : " + balanceAcc2)
 
 /*
 - The process of inheritance is to make DevAccount class can call function/method that is declare in parent class.
 - The set balance method is to update balance when money in.
 - The get balance method is to display current balance that available for the current account.
-- 
+- Transfer method is to transfer balance from first account to another account and set the new balance for the other account.
 
 */
